9ac728d30030a7a4df18702d91c8094952d0f6d1
GauravsSharma <gauravsharma16072001@gmail.com>
2024-08-15 15:48:21 +0530

    isAuthenticated added

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 const { register, login, getUser } = require("../controller/User");
 const express = require("express");
+const { isAuthenticated } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/getuser").get(getUser);
+router.route("/getuser").get(isAuthenticated,getUser);
 
 module.exports = router;
