0ad14a88d3eeb27b5286f38e7a014f63e852a510
GauravsSharma <gauravsharma16072001@gmail.com>
2024-10-14 09:33:11 +0530

    added cors options

--- server/app.js
+++ server/app.js
@@ -7,7 +7,13 @@ const project = require("./routes/project");
 const skill = require("./routes/skill");
 require('dotenv').config({ path: './config/config.env' });
 
-app.use(cors());
+const corsOptions = {
+    origin:'https://gaurav-sharma-mern-portfolio.netlify.app', // allow requests from this domain
+    credentials: true, // allow credentials (cookies, headers, etc.) to be sent
+    optionsSuccessStatus: 200, // some browsers (legacy browsers) choke on 204
+  };
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser({ sameSite: 'none' }));
 app.use(express.urlencoded({ extended: true }));
