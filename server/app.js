60f579373808b93d8fdebad1b2109a1a61adab86
GauravsSharma <gauravsharma16072001@gmail.com>
2024-10-14 09:53:05 +0530

    remove / route

--- server/app.js
+++ server/app.js
@@ -8,23 +8,20 @@ const skill = require("./routes/skill");
 require('dotenv').config({ path: './config/config.env' });
 
 const corsOptions = {
-    origin:'https://gaurav-sharma-mern-portfolio.netlify.app', // allow requests from this domain
-    credentials: true, // allow credentials (cookies, headers, etc.) to be sent
-    optionsSuccessStatus: 200, // some browsers (legacy browsers) choke on 204
-  };
+    origin: 'https://gaurav-sharma-mern-portfolio.netlify.app', // allow requests from this domain
+    credentials: true,
+    optionsSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser({ sameSite: 'none' }));
 app.use(express.urlencoded({ extended: true }));
 
+// Define your API routes here first
 app.use("/api/v1", user);
-app.use("/api/v1",project)
-app.use("/api/v1",skill)
-app.use("/", (req, res) => {
-    res.json({
-        message: "Server running"
-    });
-});
+app.use("/api/v1", project);
+app.use("/api/v1", skill);
+
 
 module.exports = app;
